fd8f0ee1ffb9f17ec59284da3c9c46e9019fe8e7
max <killme06151@gmail.com>
2025-07-07 21:41:26 +0800

    第一次上傳

---
+++ OneDrive/桌面/web project/data/database.js
@@ -0,0 +1,14 @@
+// database.js
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./data.db');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS items (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL
+    )
+  `);
+});
+
+module.exports = db;
